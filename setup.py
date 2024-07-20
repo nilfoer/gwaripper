7e4d6e6a28d278032371c815f410cd4457c51f1d
nilfoer <60221153+nilfoer@users.noreply.github.com>
2024-07-20 02:57:50 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ for dirpath, dirnames, filenames in os.walk(os.path.abspath('gwaripper_webGUI'))
 
 setuptools.setup(
     name="GWARipper",
-    version="0.8.0",
+    version="0.9.0",
     description="A script that downloads audio files from the gonewildaudio subreddit.",
     long_description=long_description,
     long_description_content_type="text/markdown",
